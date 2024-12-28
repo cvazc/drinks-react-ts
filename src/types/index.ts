26c938d2f121e54668c128d5fa2d0ed2d4d32f3f
Sebastian <contreras.sebastian.dev@gmail.com>
2024-12-27 19:24:07 -0600

    feat: add DrinkCard component and update IndexPage to display drinks with conditional rendering

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,12 @@
 import { z } from "zod"
-import { CategoriesAPIResponseSchema, DrinksAPIREsponse, SearchFilterSchema } from "../utils/recipes-schema"
+import {
+    CategoriesAPIResponseSchema,
+    DrinkAPIREsponse,
+    DrinksAPIREsponse,
+    SearchFilterSchema,
+} from "../utils/recipes-schema"
 
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>
 export type SearchFilter = z.infer<typeof SearchFilterSchema>
 export type Drinks = z.infer<typeof DrinksAPIREsponse>
+export type Drink = z.infer<typeof DrinkAPIREsponse>
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,9 +1,25 @@
-
+import { useMemo } from "react"
+import { useAppStore } from "../stores/useAppStore"
+import DrinkCard from "../components/DrinkCard"
 
 export default function IndexPage() {
+    const drinks = useAppStore((state) => state.drinks)
+    const hasDrinks = useMemo(() => drinks.drinks.length, [drinks])
     return (
         <>
-            <h1>Inicio</h1>
+            <h1 className="text-6xl font-extrabold">Recetas</h1>
+
+            {hasDrinks ? (
+                <div className="grid grid-cols-1 gap-10 my-10 md:grid-cols-2 2xl:grid-cols-3">
+                    {drinks.drinks.map((drink) => (
+                        <DrinkCard key={drink.idDrink} drink={drink} />
+                    ))}
+                </div>
+            ) : (
+                <p className="my-10 text-2xl text-center">
+                    No hay resultados aún
+                </p>
+            )}
         </>
     )
 }
