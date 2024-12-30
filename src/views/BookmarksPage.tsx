168104e40cfc80eb79d12cca0560260aaba112ca
Sebastian <contreras.sebastian.dev@gmail.com>
2024-12-30 08:41:01 -0600

    feat: enhance BookmarksPage to display favorite drinks and handle empty state

--- src/views/BookmarksPage.tsx
+++ src/views/BookmarksPage.tsx
@@ -1,7 +1,26 @@
+import { useMemo } from "react"
+import DrinkCard from "../components/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
+
 export default function BookmarksPage() {
+    const favorites = useAppStore((state) => state.favorites)
+
+    const hasBookmarks = useMemo(() => favorites.length, [favorites])
+
     return (
         <>
-            <h1>Favoritos</h1>
+            <h1 className="text-6xl font-extrabold">Favoritos</h1>
+            {hasBookmarks ? (
+                <div className="grid grid-cols-1 gap-10 my-10 md:grid-cols-2 2xl:grid-cols-3">
+                    {favorites.map((drink) => (
+                        <DrinkCard key={drink.idDrink} drink={drink} />
+                    ))}
+                </div>
+            ) : (
+                <p className="my-10 text-2xl text-center">
+                    Los favoritos se mostraran aquí
+                </p>
+            )}
         </>
     )
 }
