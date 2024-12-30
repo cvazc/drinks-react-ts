a92afa13f41562e714f63e12f96cceb3292162ad
Sebastian <contreras.sebastian.dev@gmail.com>
2024-12-30 08:33:21 -0600

    feat: add loadFromStorage functionality to bookmarkSlice and integrate it in Layout component

--- src/stores/bookmarkSlice.ts
+++ src/stores/bookmarkSlice.ts
@@ -1,16 +1,20 @@
 import { StateCreator } from "zustand"
 import { Recipe } from "../types"
+import { createRecipesSlice, RecipesSliceType } from "./recipeSlice"
 
 export type BookmarksSliceType = {
     favorites: Recipe[]
     handleClickBookmark: (recipe: Recipe) => void
     bookmarkExist: (id: Recipe["idDrink"]) => boolean
+    loadFromStorage: () => void
 }
 
-export const createBookmarksSlice: StateCreator<BookmarksSliceType> = (
-    set,
-    get
-) => ({
+export const createBookmarksSlice: StateCreator<
+    BookmarksSliceType & RecipesSliceType,
+    [],
+    [],
+    BookmarksSliceType
+> = (set, get, api) => ({
     favorites: [],
     handleClickBookmark: (recipe) => {
         if (get().bookmarkExist(recipe.idDrink)) {
@@ -24,8 +28,18 @@ export const createBookmarksSlice: StateCreator<BookmarksSliceType> = (
                 favorites: [...state.favorites, recipe],
             }))
         }
+        createRecipesSlice(set, get, api).closeModal()
+        localStorage.setItem("favorites", JSON.stringify(get().favorites))
     },
     bookmarkExist: (id) => {
         return get().favorites.some((favorite) => favorite.idDrink === id)
+    },
+    loadFromStorage: () => {
+        const storedBookmarks = localStorage.getItem("favorites")
+        if (storedBookmarks) {
+            set({
+                favorites: JSON.parse(storedBookmarks),
+            })
+        }
     },
 })
