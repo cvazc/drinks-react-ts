65222492e82088f78276bdaeb852df611467873b
Sebastian <contreras.sebastian.dev@gmail.com>
2025-01-08 16:13:19 -0600

    feat: implement lazy loading for IndexPage and BookmarksPage with Suspense

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,32 @@
+import { lazy, Suspense } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import IndexPage from "./views/IndexPage"
-import BookmarksPage from "./views/BookmarksPage"
 import Layout from "./layouts/Layout"
+
+const IndexPage = lazy(() => import("./views/IndexPage"))
+const BookmarksPage = lazy(() => import("./views/BookmarksPage"))
 
 export default function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<Layout />}>
-                    <Route path="/" element={<IndexPage />} index />
-                    <Route path="/bookmarks" element={<BookmarksPage />} />
+                    <Route
+                        path="/"
+                        element={
+                            <Suspense fallback="Cargando...">
+                                <IndexPage />
+                            </Suspense>
+                        }
+                        index
+                    />
+                    <Route
+                        path="/bookmarks"
+                        element={
+                            <Suspense fallback="Cargando...">
+                                <BookmarksPage />
+                            </Suspense>
+                        }
+                    />
                 </Route>
             </Routes>
         </BrowserRouter>
